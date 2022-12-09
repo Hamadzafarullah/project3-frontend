7965ebb1f51d82dcbed58da41cbeab1a58c8ef5f
sky0815a <sky0815a@gmail.com>
2022-12-09 10:25:18 -0500

    directory fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import Header from './Header';
-import Search from './Search';
-import Location from './Location';
-import Footer from './Footer';
-import Travel from './Travel';
+// import Header from './Header';
+// import Search from './Search';
+// import Location from './Location';
+import Footer from './components/footer';
+import Travel from './components/travel';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Search />
+      {/* <Header />
+      <Search /> */}
       <Travel />
-      <Location/>
       {/* <Location /> */}
       <Footer />
     </div>
