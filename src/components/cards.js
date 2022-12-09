bc705ee5128c8266c660b1677b2fe130297fd165
jay123son <youremail@domain.com>
2022-12-09 09:34:41 -0500

    more files

---
+++ src/components/cards.js
@@ -0,0 +1,161 @@
+import Travel from "./Travelapp.js"
+
+
+function cards(){
+  return(
+    <div className="cardbox">
+      <div className="cards">
+      <h2>{`${travelapp[0].image}`}</h2>
+      <p>{`${travelapp[0].location}`}</p>
+        <p>{`${travelapp[0].cost}`}</p>
+        <p>{`${travelapp[0].attreactions}`}</p>
+        <p>{`${travelapp[0].information}`}</p>
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[1].image}`}</h2>
+      <p>{`${travelapp[1].location}`}</p>
+        <p>{`${travelapp[1].cost}`}</p>
+        <p>{`${travelapp[1].attreactions}`}</p>
+        <p>{`${travelapp[1].information}`}</p>
+         
+         
+            </div>
+
+
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[2].image}`}</h2>
+      <p>{`${travelapp[2].location}`}</p>
+        <p>{`${travelapp[2].cost}`}</p>
+        <p>{`${travelapp[2].attreactions}`}</p>
+        <p>{`${rtravelapp[2].information}`}</p>
+         
+         
+            </div>
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[3].image}`}</h2>
+      <p>{`${travelapp[3].location}`}</p>
+        <p>{`${travelapp[3].cost}`}</p>
+        <p>{`${travelapp[3].attreactions}`}</p>
+        <p>{`${travelapp[3].information}`}</p>
+         
+         
+            </div>
+
+
+            <div className="cards">
+      <h2>{`${travelapp[4].image}`}</h2>
+      <p>{`${travelapp[4].location}`}</p>
+        <p>{`${travelapp[4].cost}`}</p>
+        <p>{`${travelapp[4].attreactions}`}</p>
+        <p>{`${travelapp[4].information}`}</p>
+         
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[5].image}`}</h2>
+      <p>{`${travelapp[5].location}`}</p>
+        <p>{`${travelapp[5].cost}`}</p>
+        <p>{`${travelapp[5].attreactions}`}</p>
+        <p>{`${travelapp[5].information}`}</p>
+         
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[6].image}`}</h2>
+      <p>{`${travelapp[6].location}`}</p>
+        <p>{`${travelapp[6].cost}`}</p>
+        <p>{`${travelapp[6].attreactions}`}</p>
+        <p>{`${travelapp[6].information}`}</p>
+         
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[7].image}`}</h2>
+      <p>{`${travelapp[7].location}`}</p>
+        <p>{`${travelapp[7].cost}`}</p>
+        <p>{`${travelapp[7].attreactions}`}</p>
+        <p>{`${travelapp[7].information}`}</p>
+         
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[8].image}`}</h2>
+      <p>{`${travelapp[8].location}`}</p>
+        <p>{`${travelapp[8].cost}`}</p>
+        <p>{`${travelapp[8].attreactions}`}</p>
+        <p>{`${travelapp[8].information}`}</p>
+         
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[9].image}`}</h2>
+      <p>{`${travelapp[9].location}`}</p>
+        <p>{`${travelapp[9].cost}`}</p>
+        <p>{`${travelapp[9].attreactions}`}</p>
+        <p>{`${travelapp[9].information}`}</p>
+         
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${travelapp[10].image}`}</h2>
+      <p>{`${travelapp[10].location}`}</p>
+        <p>{`${travelapp[10].cost}`}</p>
+        <p>{`${travelapp[10].attreactions}`}</p>
+        <p>{`${travelapp[10].information}`}</p>
+         
+         
+            </div>
+            
+            <div className="cards">
+      <h2>{`${travelapp[11].image}`}</h2>
+      <p>{`${travelapp[11].location}`}</p>
+        <p>{`${travelapp[11].cost}`}</p>
+        <p>{`${travelapp[11].attreactions}`}</p>
+        <p>{`${travelapp[11].information}`}</p>
+         
+         
+            </div>
+            
+
+            </div>
+            
+            
+  )}
+ 
+
+
+  export default cards;
