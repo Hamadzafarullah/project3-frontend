c4d49d4549ca5c08c770ccbf08e31f56614b64b2
sky0815a <sky0815a@gmail.com>
2022-12-09 09:56:13 -0500

    clearing things up

--- src/components/title.js
+++ src/components/title.js
@@ -1,1 +1,14 @@
-TBA
+import React from 'react';
+import Navbar from './Navbar';
+
+
+
+const title = () => {
+  return (
+    <div>
+      <h2>Look for your dream vaccation</h2>
+      <Navbar />
+    </div>
+  );
+}
+export default title
