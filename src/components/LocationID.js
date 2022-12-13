6ff11807048e8afe802c4880ce834d62c22a66bc
HamadZafarullah <hamadzafarullah@gmail.com>
2022-12-12 21:13:14 -0500

    delete is done worked w jayson/jihwan i piolted

--- src/components/LocationID.js
+++ src/components/LocationID.js
@@ -1,20 +1,59 @@
 import React from "react";
-import { useParams } from "react-router";
 import { useState } from "react";
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom' 
 
-export default function LocationId() {
+export default function LocationId(props) {
     const {id}= useParams()
     const [LocationId, setLocationId] = useState([]); 
+    
+    const URL =`http://localhost:8000/location/${id}`
+    const getLoca = async () => {
+        console.log(URL)
 
+        try {
+
+            const response = await fetch(URL)
+            const locationData = await response.json()
+
+            setLocationId(locationData)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const navigate = useNavigate()
+
+    const removeLocation = async () => {
+        try {
+          const options = {
+              method:"DELETE"
+          }
+
+          const response = await fetch(URL, options)
+
+
+          const deletedLocation = await response.json()
+
+
+          navigate('/')
+
+
+
+      } catch (err) {
+          console.log(err)
+          navigate(URL)
+      }
+  }
     useEffect(() => {
-        axios.get(`http://localhost:8000/location/${id}`)
-          .then((response) => response.data)
-          .then((response) => {
-            console.log(response)
-            setLocationId(response) 
-          })
+        getLoca()
+        // axios.get(`http://localhost:8000/location/${id}`)
+        //   .then((response) => response.data)
+        //   .then((response) => {
+        //     console.log(response)
+        //     setLocationId(response) 
+        //   })
     }, []);
 
     return (
@@ -27,6 +66,11 @@ export default function LocationId() {
                         <div>{LocationId.information}</div>
                         <div>{LocationId.attractions}</div>
                         <div>{LocationId.cost}</div>
+                        <div>
+                      <button className="delete" onClick={removeLocation}>
+                                    Remove Location
+                                </button>
+            </div>
                     </div>
                 {/* )
             })} */}
--- src/components/addLoca.js
+++ src/components/addLoca.js
@@ -1,42 +1,78 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react'
 
-export default function Addlocation() {
-    const [location, setLocation] = useState("")
-    const [cost, setCost] = useState("")
-    const [attractions, setAttractions] = useState("")
-    const [images, setImages] = useState("")
-    const [information, setInformation] = useState("")
-   
 
+
+ function Addlocation() {
+    const BASE_URL = 'http://localhost:8000/location'
+    const [newForm, setNewForm]= useState({
+
+    location: "",
+    cost: "",
+    attractions: "",
+    images: "",
+    information: "",
+
+   })
+   const getLocation = async (fn) => {
+    try {
+        const response = await fetch(BASE_URL);
+        const allLocation = await response.json();
+
+        fn(allLocation);
+    } catch (err) {
+        console.log(err);
+    }
+};
+   const addLoca = async (locationData)=>{
+    try {
+        const newLocation = await fetch (BASE_URL, {
+            method:"post",
+            headers:{
+                "Content-Type":"application-json",
+            },
+            body:JSON.stringify(locationData)
+        })
+        getLocation()
+    }catch(err){
+        console.log(err)
+    }
+   }
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(JSON.stringify({
-            location: location,
-            cost: cost,
-            attractions: attractions,
-            images: images,
-            information: information,
-        }))
-       
+        const newLoca= await addLoca()
+        setNewForm ({
+            location: "",
+            cost: "",
+            attractions: "",
+            images: "",
+            information: "",
+        })
+      
     }
+    const handleChange = (e)=>{
+        // console.log("current input",e.target.name)
+        const data = {...newForm, [e.target.name]: e.target.value}
+        setNewForm(data)
+       }
     return (
         <div>
             <h2>Add a Location:</h2>
             <form onSubmit={handleSubmit}>
                 <label for="location">Location:</label>
-                <input type="text" id="location" location="location" onChange={(e) => setLocation(e.target.value)} />
+                <input type="text" value={newForm.location}  name="location" onChange={handleChange} />
                 <br />
                 <label for="cost">Cost:</label>
-                <input type="text" id="cost" location="cost" onChange={(e) => setCost(e.target.value)} />
+                <input type="text" value={newForm.cost} name="cost" onChange={handleChange} />
                 <br />
                 <label for="attractions">Attractions:</label>
-                <input type="text" id="attractions" location="attractions" onChange={(e) => setAttractions(e.target.value)} />
+                <input type="text" value={newForm.attractions} name="attractions" onChange={handleChange} />
                 <br />
                 <label for="images">Images:</label>
-                <input type="text" id="images" location="images" onChange={(e) => setImages(e.target.value)} />
+                <input type="text" value={newForm.images} name="images" onChange={handleChange} />
                 <br />
                 <label for="information">Information:</label>
-                <input type="text" id="information" location="information" onChange={(e) => setInformation(e.target.value)} />
+                <input type="text" value={newForm.information} name="information" onChange={handleChange} />
                 <br />
                
                 <br /><br />
@@ -45,7 +81,7 @@ export default function Addlocation() {
         </div>
     )
 }
-
+export default Addlocation
 
 
 
